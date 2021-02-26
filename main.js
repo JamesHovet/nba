c9adb9028a4a089d04d9daabcf7696fb6fd3f780
James Hovet <jrhovet@gmail.com>
2021-02-25 18:33:35 -0600

    progressive loading!!!

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var width = 1200;
 var height = 800;
+var resolution = 80;
+var boxWidth = (height / resolution)
+var boxMargin = 0.0;
+var boxInnerWidth = boxWidth - 2 * boxMargin;
+
+var sliceSize = 50000;
 
 var chart = d3.select("canvas");
 chart.attr("width", width)
     .attr("height", height)
 var context = chart.node().getContext("2d");
-var svg = d3.select("svg");
+var svgRaw = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+var svg = svgRaw.append("g")
+
+var court = svg.append("g").attr("id", "court");
+var progressBar = svg.append("g").attr("id", "progressBar");
 
 var detachedContainer = document.createElement("custom");
 var dataContainer = d3.select(detachedContainer);
@@ -27,6 +40,31 @@ var col = {
 var idToTeams = {55: 'PHI', 38: 'BOS', 44: 'GSW', 60: 'OKC', 49: 'MIL', 66: 'CHA', 51: 'BKN', 65: 'DET', 54: 'IND', 63: 'MEM', 48: 'MIA', 53: 'ORL', 37: 'ATL', 52: 'NYK', 61: 'TOR', 39: 'CLE', 40: 'NOP', 45: 'HOU', 59: 'SAS', 50: 'MIN', 58: 'SAC', 62: 'UTA', 46: 'LAC', 43: 'DEN', 42: 'DAL', 56: 'PHX', 41: 'CHI', 64: 'WAS', 47: 'LAL', 57: 'POR'}
 var teamsToIds = {'PHI': 55, 'BOS': 38, 'GSW': 44, 'OKC': 60, 'MIL': 49, 'CHA': 66, 'BKN': 51, 'DET': 65, 'IND': 54, 'MEM': 63, 'MIA': 48, 'ORL': 53, 'ATL': 37, 'NYK': 52, 'TOR': 61, 'CLE': 39, 'NOP': 40, 'HOU': 45, 'SAS': 59, 'MIN': 50, 'SAC': 58, 'UTA': 62, 'LAC': 46, 'DEN': 43, 'DAL': 42, 'PHX': 56, 'CHI': 41, 'WAS': 64, 'LAL': 47, 'POR': 57}
 
+const emptySquares = function() {return new Array(resolution).fill(0).map(() => new Array(resolution).fill(0))}
+const emptyHist = function() {return new Array(35).fill(0);}
+
+var rows = undefined;
+
+var attempts = emptySquares()
+var pts = emptySquares()
+var ratio = emptySquares()
+
+var attemptsHist = emptyHist()
+var ptsHist = emptyHist()
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// Utils
+//----------------------------------------------------------------------------------------------------------------------
+
+// copied from http://bl.ocks.org/joshcarr/e447e5c953671a4729d8
+var requestAnim = window.requestAnimationFrame
+|| window.webkitRequestAnimationFrame
+|| window.mozRequestAnimationFrame
+|| window.oRequestAnimationFrame
+|| window.msRequestAnimationFrame
+|| function(callback) { setTimeout(callback, 17); };
+
 //----------------------------------------------------------------------------------------------------------------------
 // setup scales etc.
 //----------------------------------------------------------------------------------------------------------------------
@@ -34,41 +72,114 @@ const pyRange = (start, stop, step = 1) =>
   Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
 var scaleX = d3.scaleQuantize()
     .domain([-250, 250])
-    .range(pyRange(0, 800, 10))
+    .range([...Array(resolution).keys()])
+    // .range(pyRange(0, 800, 10))
 
 var scaleY = d3.scaleQuantize()
     .domain([-50, 450])
-    .range(pyRange(0, 800, 10))
- 
+    .range([...Array(resolution).keys()])
+    // .range(pyRange(0, 800, 10))
+
+var scaleHeatmap = d3.scaleQuantile()
+    .range(d3.interpolateBlues)
+
+
+distanceBins = d3.bin().domain([0, 35]).thresholds(35)([])
+
+
   
 //----------------------------------------------------------------------------------------------------------------------
 // Parse new data
 //----------------------------------------------------------------------------------------------------------------------
 
-function ready(compiled) {
-    // compiled = compiled.slice(compiled.indexOf("\n"))
-    compiled = compiled.split("\n")
-    const rows = compiled.map((el) => {return el.split(',').map(d => +d)})
-    console.log(rows)
-    var dataBinding = dataContainer.selectAll("custom.rect")
-        .data(rows)
+function processSlice(slice, filter) {
+    slice.forEach(d => {
+        let x = scaleX(d[col.LOC_X]);
+        let y = scaleY(d[col.LOC_Y]);
+        attempts[y][x] += 1;
+        pts[y][x] += d[col.SHOT_MADE_FLAG];
+        let dist = Math.min(Math.floor(d[col.SHOT_DISTANCE]),34);
+        attemptsHist[dist] += 1;
+        ptsHist[dist] += d[col.SHOT_MADE_FLAG];
+    });
+    ratio = ratio.map((row, rowI) => row.map((el, colI) => pts[rowI][colI] / attempts[rowI][colI]))
+}
 
-        dataBinding.enter()
-        .append("custom")
-        .filter((d) => {
-            return d[col.SEASON] == 2011 && d[col.TEAM_ID] == teamsToIds.BOS;
-        })
-        .classed("rect", true)
-        .attr("x", (d) => scaleX(d[col.LOC_X]))
-        .attr("y", (d) => scaleY(d[col.LOC_Y]))
-        .attr("size", 8)
-        .attr("fillStyle", (d) => {if(d[col.SHOT_MADE_FLAG]){
-            return "rgba(0, 255, 0, 0.02)"
-        } else {
-            return "rgba(255, 0, 0, 0.02)"
-        }});
+function ready(compiled) {
+    headers = compiled.slice(0, compiled.indexOf("\n"))
+    console.log(headers)
+    compiled = compiled.slice(compiled.indexOf("\n") + 1)
+    compiled = compiled.split("\n")
+    compiled.pop()
+    rows = compiled.map((el) => {return el.split(',').map(d => +d)})
+    console.log(rows)
+
+    drawLoop();    
+
+    // var dataBinding = dataContainer.selectAll("custom.rect")
+    //     .data(rows)
+
+    //     dataBinding.enter()
+    //     .append("custom")
+    //     .filter((d) => {
+    //         return d[col.SEASON] == 2011;
+    //     })
+    //     .classed("rect", true)
+    //     .attr("x", (d) => scaleX(d[col.LOC_X]))
+    //     .attr("y", (d) => scaleY(d[col.LOC_Y]))
+    //     .attr("size", 8)
+    //     .attr("fillStyle", (d) => {if(d[col.SHOT_MADE_FLAG]){
+    //         return "rgba(0, 255, 0, 0.02)"
+    //     } else {
+    //         return "rgba(255, 0, 0, 0.02)"
+    //     }});
     
-    drawCanvas()
+    // drawCanvas()
+}
+
+var currentIndex = 0;
+function drawLoop(){
+    processSlice(rows.slice(currentIndex, currentIndex + sliceSize));
+    currentIndex += sliceSize;
+
+    drawProgressBar()
+
+    drawCourt(ratio);
+    
+    requestAnim(drawLoop);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// SVG bits
+//----------------------------------------------------------------------------------------------------------------------
+
+function drawCourt(raster) {
+    scaleHeatmap = d3.scaleSequentialQuantile(raster.flat(), d3.interpolateBlues)
+    court.selectAll("rect")
+        .data(raster.flat())
+        .join("rect")
+        .attr("x", (d, i) => (i % resolution) * boxWidth + boxMargin)
+        .attr("y", (d, i) => Math.floor(i / resolution) * boxWidth + boxMargin)
+        .attr("width", boxInnerWidth)
+        .attr("height", boxInnerWidth)
+        .attr("fill", (d) => scaleHeatmap(d))
+
+}
+
+function drawProgressBar() {
+    if(currentIndex > rows.length) {
+        progressBar.attr("visibility", "hidden")
+    } else {
+        progressBar.attr("visibility", "visible")
+        progressBar.selectAll("rect")
+            .data([currentIndex])
+            .join("rect")
+            .attr("y", height - 5)
+            .attr("x", 0)
+            .attr("width", (d) => (d / rows.length) * height)
+            .attr("height", 5)
+            .attr("fill", "darkblue")
+    }
 }
 
 
@@ -83,25 +194,17 @@ function clearCanvas() {
 }
 
 function drawCanvas() {
-    var elements = dataContainer.selectAll("custom.rect");
-    elements.each(function(d) {
-      var node = d3.select(this);
-      
-      context.beginPath();
-      context.fillStyle = node.attr("fillStyle");
-      context.rect(node.attr("x"), node.attr("y"), node.attr("size"), node.attr("size"));
-      context.fill();
-      context.closePath();
-      
-    })
-  }
-  
+
+}
+
+
+
+
 //----------------------------------------------------------------------------------------------------------------------
 // start
 //----------------------------------------------------------------------------------------------------------------------
 clearCanvas();
 
-d3.text("./compiled.csv")
-    .then(ready);
-// d3.csv("./head.csv").then(ready);
+d3.text("./compiled.csv").then(ready);
+// d3.text("./head.csv").then(ready);
  
